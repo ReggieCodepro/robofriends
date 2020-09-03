4959f61f9d45a8809aa881b462ed61ff65305361
Reginald Wright <yungengine8@gmail.com>
2020-09-03 17:35:08 -0400

    Removed hello files and app files

---
+++ src/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Card = () => {
+    <div>
+        <img alt='photo' src='' />
+    
+    <div>
+        <h2>Jane Doe</h2>
+        <p>jane.doe@gmail.com </p>
+    </div>
+    </div>
+}
