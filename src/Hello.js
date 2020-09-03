60ca6b04ac69855def96740d75802d522a3841a6
Reginald Wright <yungengine8@gmail.com>
2020-09-03 03:23:22 -0400

    lol

--- src/Hello.js
+++ src/Hello.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 
 class Hello extends Component {
-    render(){
-        return <h1>Hello World</h1>
+    render() {
+      return <h1>Hello World</h1>
     }
 }
 
