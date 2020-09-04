368b1c92e11b92a1be20cb9823c5cbac7ce96f4f
Reginald Wright <yungengine8@gmail.com>
2020-09-04 00:12:22 -0400

    Added Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const Card = () => {
-    <div>
-        <img alt='photo' src='' />
+const Card = ({ name, email, id }) => {
+
+    return (
+    <div className='tc bg-light-green dib br3 ma2 grow bw2 shadow-5'>
+        <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
     
     <div>
-        <h2>Jane Doe</h2>
-        <p>jane.doe@gmail.com </p>
+        <h2>{name}</h2>
+        <p>{email}</p>
     </div>
     </div>
+    );
 }
+export default Card;
