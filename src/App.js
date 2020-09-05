970b26952de41d9d519b4e7fbd07336c77b0b062
Reginald Wright <yungengine8@gmail.com>
2020-09-05 04:46:27 -0400

    search is finished

--- src/App.js
+++ src/App.js
@@ -14,16 +14,19 @@ class App extends Component {
             searchfield: ''
         }
     }
-    onSearchChange(event) {
-        console.log(event.target.value);
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
     }
 
     render(){
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
     return (
         <div className='tc'>
           <h1>RoboFriends </h1>
-          <SearchBox SearchChange={this.onSearchChange} />
-          <CarList robots={this.state.robots}/>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CarList robots={filteredRobots}/>
         </div>
     );
   }
