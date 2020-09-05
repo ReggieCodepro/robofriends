6958798fd9f1ba65e010165c98939f6b14b25ce0
Reginald Wright <yungengine8@gmail.com>
2020-09-05 04:06:18 -0400

    add onChange and searchChange

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import CarList from './CardList'
 import SearchBox from './SearchBox'
 import { robots } from './robots';
 //State: is an object that describe //props come out of state
-const state = { 
-    robots: robots,
-    searchfield: ''
 
-}
 
 
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots, 
+            searchfield: ''
+        }
+    }
+    onSearchChange(event) {
+        console.log(event.target.value);
+    }
+
     render(){
     return (
         <div className='tc'>
           <h1>RoboFriends </h1>
-          <SearchBox />
-          <CarList robots={robots}/>
+          <SearchBox SearchChange={this.onSearchChange} />
+          <CarList robots={this.state.robots}/>
         </div>
     );
   }
