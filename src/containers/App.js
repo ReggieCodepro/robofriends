b0eec6ae34e3093d1d9a89cc43f20af782ec6452
yungengine8 <yungengine8@gmail.com>
2020-09-13 05:28:03 -0400

    finish project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import CarList from '..Components/CardList'
-import SearchBox from '.Components./SearchBox'
-import { robots } from './robots';
-import Scroll from '.Components./Scroll';
-import './containers/App.css'
+import CarList from '../Components/CardList'
+import SearchBox from '../Components/SearchBox'
+import Scroll from '../Components/Scroll';
+import './App.css'
 //State: is an object that describe //props come out of state
 
 
@@ -12,28 +11,38 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots, 
+            robots: [], 
             searchfield: ''
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users=>{this.setState({robots: users})});
     }
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
     }
 
     render(){
-        const filteredRobots = this.state.robots.filter(robots =>{
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-    return (
-        <div className='tc'>
-          <h1 className='f1'>RoboFriends </h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-          <CarList robots={filteredRobots}/>
-          </Scroll>
+        return !robots.length ?
+             <h1> Loading</h1> :
+             (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends </h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
+                <CarList robots={filteredRobots}/>
+                </Scroll>
         </div>
     );
   }
-}
+ }
+
 
 export default App;
